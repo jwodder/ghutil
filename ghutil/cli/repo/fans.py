c4980a37b780cbf3e2c8342c3e1280c62284ea24
John T. Wodder II <git@varonathe.org>
2017-05-20 19:35:37 +0000

    Added a `gh repo fans` command

---
+++ ghutil/cli/repo/fans.py
@@ -0,0 +1,13 @@
+import click
+from   ghutil.repos   import GHRepo, get_remote_url
+from   ghutil.showing import print_json
+
+@click.command()
+@click.argument('repo', type=GHRepo(), default=get_remote_url)
+@click.pass_obj
+def cli(gh, repo):
+    """ List forkers & stargazers """
+    print_json({
+        "forkers": [f["owner"]["login"] for f in repo.forks.get()],
+        "stargazers": [s["login"] for s in repo.stargazers.get()],
+    })
