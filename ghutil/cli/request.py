670f0356832355f1bc21b93c2d29a7f998344c1a
John T. Wodder II <git@varonathe.org>
2017-05-25 17:46:23 +0000

    Gave `gh request` an `--accept` option

--- ghutil/cli/request.py
+++ ghutil/cli/request.py
@@ -2,16 +2,20 @@ import click
 from   ghutil.showing import print_json
 
 @click.command()
+@click.option('-a', '--accept', multiple=True)
+@click.option('-d', '--data')
 @click.option(
     '-X', '--method',
     type=click.Choice(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
     default='GET',
 )
-@click.option('-d', '--data')
 @click.argument('path')
 @click.pass_obj
-def cli(gh, path, method, data):
+def cli(gh, path, method, accept, data):
     """ Make an arbitrary API request """
-    ret = gh[path][method](data=data)
+    headers = {}
+    if accept:
+        headers["Accept"] = '; '.join(accept)
+    ret = gh[path][method](data=data, headers=headers)
     if ret is not None:
         print_json(ret)
