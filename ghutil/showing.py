b7b413694c605a83e9356b506c5867f5b0bd9c95
John T. Wodder II <git@varonathe.org>
2017-06-03 21:36:08 +0000

    Stop repeating myself in showing.py

--- ghutil/showing.py
+++ ghutil/showing.py
@@ -8,75 +8,92 @@ def print_json(obj, err=False):
     click.echo(json.dumps(obj, sort_keys=True, indent=4, ensure_ascii=False),
                err=err)
 
-def maybe_user(obj):
-    return None if obj is None else obj["login"]
+def show_fields(*fields):
+    def show(obj):
+        about = {}
+        for entry in fields:
+            if isinstance(entry, str):
+                entry = (entry,)
+            name, *subpath = entry
+            try:
+                value = obj[name]
+            except KeyError:
+                continue
+            for sp in subpath:
+                if value is None:
+                    break
+                elif callable(sp):
+                    value = sp(value)
+                elif isinstance(value, list):
+                    value = [v and v[sp] for v in value]
+                else:
+                    value = value[sp]
+            about[name] = value
+        return about
+    return show
 
-def repo_info(repo):
-    return {
-        ### TODO: Include fork information
-        "owner": maybe_user(repo["owner"]),
-        "name": repo["name"],
-        "url": repo["url"],
-        "html_url": repo["html_url"],
-        "clone_url": repo["clone_url"],
-        "git_url": repo["git_url"],
-        "ssh_url": repo["ssh_url"],
-        "full_name": repo["full_name"],
-        "description": repo["description"],
-        "homepage": repo["homepage"],
-        "private": repo["private"],
-        "default_branch": repo["default_branch"],
-        "created_at": repo["created_at"],
-        "updated_at": repo["updated_at"],
-        "pushed_at": repo["pushed_at"],
-        "fork": repo["fork"],
-        "forks_count": repo["forks_count"],
-        "watchers_count": repo["watchers_count"],
-        "size": repo["size"],
-        "subscribers_count": repo["subscribers_count"],
-        "stargazers_count": repo["stargazers_count"],
-        "id": repo["id"],
-        "language": repo["language"],
-        "network_count": repo["network_count"],
-        "open_issues_count": repo["open_issues_count"],
-    }
+repo_info = show_fields(
+    ### TODO: Include fork information
+    ("owner", "login"),
+    "name",
+    "url",
+    "html_url",
+    "clone_url",
+    "git_url",
+    "ssh_url",
+    "full_name",
+    "description",
+    "homepage",
+    "private",
+    "default_branch",
+    "created_at",
+    "updated_at",
+    "pushed_at",
+    "fork",
+    "forks_count",
+    "watchers_count",
+    "size",
+    "subscribers_count",
+    "stargazers_count",
+    "id",
+    "language",
+    "network_count",
+    "open_issues_count",
+)
 
-def gist_info(gist):
-    return {
-        "id": gist["id"],
-        "url": gist["url"],
-        "git_push_url": gist["git_push_url"],
-        "files": {
-            fname: {k:v for k,v in about.items() if k != 'content'}
-            for fname, about in gist["files"].items()
-        },
-        "public": gist["public"],
-        "html_url": gist["html_url"],
-        "owner": maybe_user(gist["owner"]),
-        "description": gist["description"],
-        "created_at": gist["created_at"],
-        "updated_at": gist["updated_at"],
-    }
+gist_info = show_fields(
+    "id",
+    "url",
+    "git_push_url",
+    ("files", lambda files: {
+        fname: {k:v for k,v in about.items() if k != 'content'}
+        for fname, about in files.items()
+    }),
+    "public",
+    "html_url",
+    ("owner", "login"),
+    "description",
+    "created_at",
+    "updated_at",
+)
 
-def issue_info(issue):
-    return {
-        "assignee": maybe_user(issue["assignee"]),
-        "assignees": list(map(maybe_user, issue["assignees"])),
-        "closed_at": issue["closed_at"],
-        "closed_by": maybe_user(issue["closed_by"]),
-        "comments": issue["comments"],
-        "created_at": issue["created_at"],
-        "html_url": issue["html_url"],
-        "id": issue["id"],
-        "labels": [l["name"] for l in issue["labels"]],
-        "locked": issue["locked"],
-        "milestone": issue["milestone"]["title"] if issue["milestone"] is not None else None,
-        "number": issue["number"],
-        "state": issue["state"],
-        "title": issue["title"],
-        "updated_at": issue["updated_at"],
-        "url": issue["url"],
-        "user": maybe_user(issue["user"]),
-        "repository_url": issue["repository_url"],
-        ### pull_request
-    }
+issue_info = show_fields(
+    ("assignees", "login"),
+    "closed_at",
+    ("closed_by", "login"),
+    "comments",
+    "created_at",
+    "html_url",
+    "id",
+    ("labels", "name"),
+    "locked",
+    ("milestone", "title"),
+    "number",
+    "state",
+    "title",
+    "updated_at",
+    "url",
+    ("user", "login"),
+    "repository_url",
+    ### pull_request
+)
