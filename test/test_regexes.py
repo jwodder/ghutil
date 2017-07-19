100f17f17fcf4b6a39676ade21336d738b1af948
John T. Wodder II <git@varonathe.org>
2017-07-19 20:05:02 +0000

    Test GH_REPO_RGX

--- test/test_regexes.py
+++ test/test_regexes.py
@@ -1,6 +1,6 @@
 import re
 import pytest
-from   ghutil.repos import GH_USER_RGX
+from   ghutil.repos import GH_USER_RGX, GH_REPO_RGX
 
 @pytest.mark.parametrize('name', [
     'steven-universe',
@@ -25,6 +25,48 @@ def test_good_users(name):
     'steven--universe',
     's--u',
     '-',
+    '',
 ])
 def test_bad_users(name):
     assert re.match('^{}$'.format(GH_USER_RGX), name) is None
+
+@pytest.mark.parametrize('repo', [
+    'steven-universe',
+    'steven',
+    's',
+    's-u',
+    '7152',
+    's-t-e-v-e-n',
+    's-t-eeeeee-v-e-n',
+    'peridot-2F5L-5XG',
+    '...',
+    '-steven',
+    'steven-'
+    '-steven-',
+    'steven.universe',
+    'steven_universe',
+    'steven--universe',
+    's--u',
+    'git.steven',
+    'steven.git.txt',
+    'steven.gitt',
+    '.gitt',
+    'git',
+    '-',
+    '_',
+])
+def test_good_repos(repo):
+    assert bool(re.match('^{}$'.format(GH_REPO_RGX), repo))
+
+@pytest.mark.parametrize('repo', [
+    'steven-univerß',
+    '.',
+    '..',
+    '...git',
+    '..git',
+    '.git',
+    '',
+    'steven.git',
+])
+def test_bad_repos(repo):
+    assert re.match('^{}$'.format(GH_REPO_RGX), repo) is None
