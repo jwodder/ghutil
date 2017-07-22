685afd88366d9a10f8eef245f9c544d644cf61ee
John T. Wodder II <git@varonathe.org>
2017-07-22 20:26:36 +0000

    I missed a spot.

--- ghutil/cli/repo/list_forks.py
+++ ghutil/cli/repo/list_forks.py
@@ -3,7 +3,8 @@ from   ghutil.types import Repository
 
 @click.command()
 @Repository.argument('repo')
-def cli(repo):
+@click.pass_obj
+def cli(gh, repo):
     """ List a repository's forks """
     for fork in repo.forks.get():
         click.echo(str(gh.repository(fork)))
