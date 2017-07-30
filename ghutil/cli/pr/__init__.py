e292705df44ba511a0650e5b4d70ab5f2404e412
John T. Wodder II <git@varonathe.org>
2017-07-30 20:23:01 +0000

    Document PR command line syntax

--- ghutil/cli/pr/__init__.py
+++ ghutil/cli/pr/__init__.py
@@ -4,5 +4,17 @@ from   ghutil.util import default_command, package_group
 @package_group(__package__, __file__, invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
-    """ Manage pull requests """
+    """
+    Manage pull requests
+
+    GitHub pull requests may be specified on the command line using any of the
+    following formats:
+
+    \b
+        $OWNER/$REPO/$NUMBER
+        $REPO/$NUMBER    # for repositories owned by the current user
+        $NUMBER          # for PRs of the locally-cloned repository
+        https://github.com/$OWNER/$REPO/pull/$NUMBER
+        https://api.github.com/repos/$OWNER/$REPO/pulls/$NUMBER
+    """
     default_command(ctx, 'list')
