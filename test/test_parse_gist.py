24730f2ea3930a6c5f9b1721b7e473e221044565
John T. Wodder II <git@varonathe.org>
2017-08-04 13:47:54 +0000

    Test parsing gist URLs

---
+++ test/test_parse_gist.py
@@ -0,0 +1,63 @@
+import pytest
+from   ghutil.types import Gist
+
+GIST_URLS = [
+    (
+        'https://gist.github.com/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'https://gist.github.com/4bf350e2d72b547b22dc9de52148ccbe.git',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    ('https://gist.github.com/42', {"id": "42"}),
+    (
+        'gist.github.com/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'http://gist.github.com/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'https://api.github.com/gists/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'git@gist.github.com:4bf350e2d72b547b22dc9de52148ccbe.git',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'https://gist.github.com/jwodder/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+    (
+        'https://gist.github.com/42/4bf350e2d72b547b22dc9de52148ccbe',
+        {"id": "4bf350e2d72b547b22dc9de52148ccbe"},
+    ),
+]
+
+BAD_GISTS = [
+    'https://gist.github.com/',
+    'https://github.com/4bf350e2d72b547b22dc9de52148ccbe',
+    'https://github.com/jwodder/4bf350e2d72b547b22dc9de52148ccbe',
+    'git@gist.github.com:jwodder/4bf350e2d72b547b22dc9de52148ccbe.git',
+]
+
+@pytest.mark.parametrize('arg,result', GIST_URLS + [])
+def test_parse_gist_arg(arg, result):
+    assert Gist.parse_arg(arg) == result
+
+@pytest.mark.parametrize('arg', BAD_GISTS)
+def test_parse_bad_gist_arg(arg):
+    with pytest.raises(ValueError):
+        Gist.parse_arg(arg)
+
+@pytest.mark.parametrize('url,result', GIST_URLS)
+def test_parse_gist_url(url, result):
+    assert Gist.parse_url(url) == result
+
+@pytest.mark.parametrize('url', BAD_GISTS)
+def test_parse_bad_gist_url(url):
+    with pytest.raises(ValueError):
+        Gist.parse_url(url)
